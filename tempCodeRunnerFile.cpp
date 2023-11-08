f19598596a1c5a1f4160fdbcd43cf434be253f4e
Pranjali Vaidya <91623757+PranjaliVaidya@users.noreply.github.com>
2023-11-08 19:39:28 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+void print(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<grid[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<<endl;
+}
